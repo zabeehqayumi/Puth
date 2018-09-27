b3b921b789a4321a5f4676d1d5be5539bed6b639
Zabeehullah Qayumi <32587905+zabeehqayumi@users.noreply.github.com>
2018-09-27 12:23:09 -0400

    Sign In and Sign Up pages designed with auto layout.

---
+++ Puth/SignInViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SignInViewController.swift
+//  Puth
+//
+//  Created by Zabeehullah Qayumi on 9/26/18.
+//  Copyright © 2018 Zabeehullah Qayumi. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+
+}
---
+++ Puth/SignUPViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SignUPViewController.swift
+//  Puth
+//
+//  Created by Zabeehullah Qayumi on 9/26/18.
+//  Copyright © 2018 Zabeehullah Qayumi. All rights reserved.
+//
+
+import UIKit
+
+class SignUPViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func dismiss_OnClick(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+
+}
