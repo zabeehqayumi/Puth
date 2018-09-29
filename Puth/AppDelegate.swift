50f45bc717146dd0548c276ab6d141ad1e1dd604
Zabeehullah Qayumi <32587905+zabeehqayumi@users.noreply.github.com>
2018-09-29 01:04:23 -0400

    Refactored the storyboard

--- Puth/AppDelegate.swift
+++ Puth/AppDelegate.swift
@@ -16,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        //changed the tint color of tab bar :)
+      //  UITabBar.appearance().tintColor = .black
         return true
     }
 
